cdebe5a0d819b2312391f178f287d0ca374f42ed
Shorya Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-10-02 01:04:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -5,24 +5,30 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    bool dfs(vector<int> adj[],int node,int prev,vector<int>&vis){
-        vis[node]=1;
-        for(auto it:adj[node]){
-            if(!vis[it]){
-                if(dfs(adj,it,node,vis)) return true;
+    bool bfs(vector<int> adj[],int start,vector<int>&vis){
+        queue<pair<int,int>>q;
+        q.push({start,NULL});
+        while(!q.empty()){
+            int node=q.front().first;
+            int parent=q.front().second;
+            vis[node]=1;
+            q.pop();
+            for(auto it:adj[node]){
+                if(!vis[it]) q.push({it,node});
+                else if(it!=parent) return true;
             }
-            else if(it!=prev) return true;
         }
         return false;
     }
     bool isCycle(int V, vector<int> adj[]) {
         vector<int>vis(V,0);
         for(int i=0;i<V;i++){
-            if(!vis[i] && dfs(adj,i,NULL,vis)) return true;
+            if(!vis[i] && bfs(adj,i,vis)) return true;
         }
         return false;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
